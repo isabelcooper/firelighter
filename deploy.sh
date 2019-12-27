ce57c4057426dc262a99a1d90d9583d80c547fb9
isabelcooper <isabel@triptease.com>
2019-12-27 12:23:01 +0000

    gcloud config from project name not id

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ echo ${GCLOUD_SERVICE_KEY} | base64 --decode > ${HOME}/firelighter-a9cf2a05d349.
 gcloud auth activate-service-account --key-file=${HOME}/firelighter-a9cf2a05d349.json
 echo "GOOGLE_PROJECT_ID:" ${GOOGLE_PROJECT_ID}
 echo "GOOGLE_COMPUTE_ZONE:" ${GOOGLE_COMPUTE_ZONE}
-gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
+gcloud --quiet config set project ${PROJECT_NAME}
 gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
 gcloud auth list
 
