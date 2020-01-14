80b8615b6063154d5127f7b86eacdedb7c0d4924
isabelcooper <isabel@triptease.com>
2020-01-14 16:05:35 +0000

    super simplified swagger front end

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import {Method} from "http4js/core/Methods";
 import {NativeHttpServer} from "http4js/servers/NativeHttpServer";
 import {ResOf} from "http4js/core/Res";
 import {EventHandler} from "./EventHandler";
+import * as fs from "fs";
 
 export class Server {
   private server: Routing;
@@ -12,6 +13,13 @@ export class Server {
       return ResOf(200)
     })
       .withPost('/event', eventHandler)
+      .withGet('/docs', async (_req) => {
+        return ResOf(200, (fs.readFileSync('./docs/index.html')).toString())
+      })
+      .withGet('/swagger/{filename}', async (req) => {
+        const fileType = req.uri.path().split('.')[1];
+        return ResOf(200, (fs.readFileSync(`./docs/${req.pathParams.filename}.${fileType}`)).toString())
+      })
       .asServer(new NativeHttpServer(parseInt(process.env.PORT!) || this.port));
   }
 
